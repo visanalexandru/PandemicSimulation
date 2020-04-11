d90f647f15949a639ff9d2ce3af5964a05393a35
nickk2002 <nicolae.filat@yahoo.com>
2020-04-11 11:10:15 +0300

    200 de celule rosii care se misca random

--- include/Cell.h
+++ include/Cell.h
@@ -1,20 +1,33 @@
 #ifndef CELL_H
 #define CELL_H
 #include <SFML/Graphics.hpp>
+#include <chrono>
 
 
-class Cell
+class Cell : public sf::Drawable
 {
     public:
+
         float age;
         float socialDistancing;
         float infectionProb;
+        float cellSize;
         float speed;
 
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
         Cell();
-        Cell(float _speed,float _infectProb = 1); /// default pt probabilitate e 1
+        Cell(float _size,float _speed = 1,float _infectProb = 1);
+        void Move();
         sf::Vector2f RandomDirection();
     private:
+
+        bool startMoving = false;
+        float width = 600,height = 600; /// trebuie sa vad cum iau width,height din window,e aiurea sa pun ca parametru la constructor
+        int randomTimer = rand() % 10 + 1;
+        sf::CircleShape circle;
+        sf::Vector2f direction;
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 
 
 };
--- src/Cell.cpp
+++ src/Cell.cpp
@@ -1,22 +1,70 @@
 #include "Cell.h"
 #include <iostream>
 #include <time.h>
+#include <cstdlib>
 #include "PerlinNoise.h"
+#include <cmath>
+#include <chrono>
+
+const int dx[] = {-1,-1,0,1,1,1,0,-1};/// vector de directii
+const int dy[] = {0,1,1,1,0,-1,-1,-1};
+
+using namespace std;
+using namespace chrono;
 
 Cell::Cell(){
 
 }
-Cell::Cell(float _speed,float _infectProb){
+Cell::Cell(float _size,float _speed,float _infectProb){
+    cellSize = _size;
     speed = _speed;
     infectionProb = _infectProb;
+    circle.setFillColor(sf::Color::Red);
+    circle.setRadius(cellSize);
+    circle.setPosition(rand() % (int)width,rand() % (int)height);
+}
+void Cell::draw(sf::RenderTarget &target, sf::RenderStates states) const{
+    target.draw(circle);
 }
 
 sf::Vector2f Cell::RandomDirection(){
-    PerlinNoise noise(time(NULL));
-    double randomX = noise.noise(10,10,0.8);
-    double randomY = noise.noise(10,10,0.8);
-
-    return sf::Vector2f(randomX,randomY);
+    PerlinNoise perlin;
+    float randomVal = perlin.noise(circle.getPosition().x / width,circle.getPosition().y / height, 0.8);
+    float randomX = randomVal;
+    float randomY = randomVal + 1;
+    int val = rand() % 2;
+    if(val == 0){/// sa schime directia, perlin-ul da doar valori pozitive intre [0,1]
+        randomX *= -1;
+        randomY *= -1;
+    }
+    return sf::Vector2f(randomX / 100 * speed,randomY / 100 * speed);
 }
 
+void Cell::Move(){
 
+    auto t2 = chrono::steady_clock::now();
+
+    if(startMoving == false || chrono::duration_cast<chrono::seconds>(t2 - t1).count() > randomTimer){ /// asa faci un timer in c++
+        randomTimer = rand() % 10 + 1;
+        direction = RandomDirection();
+        t1 = chrono::steady_clock::now();
+        startMoving = true;
+    }
+
+    sf::Vector2f nextPos = circle.getPosition() + direction;
+    if(nextPos.x < cellSize){
+        direction.x *= -1;
+    }
+    if(nextPos.x > width - cellSize){
+        direction.x *= -1;
+    }
+    if(nextPos.y < cellSize){
+        direction.y *= -1;
+    }
+    if(nextPos.y > height - cellSize)
+        direction.y *= -1;
+    nextPos = circle.getPosition() + direction;
+    circle.setPosition(nextPos);
+
+
+}
